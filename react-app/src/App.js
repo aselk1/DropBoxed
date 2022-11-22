fde41e126be2ec2a736d96bc51d5c853710120d8
AdamSelki <adamselki@yahoo.com>
2022-11-22 13:18:53 -0800

    react app folder

---
+++ react-app/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import NavBar from "./components/NavBar";
+import User from "./components/User";
+import UsersList from "./components/UsersList";
+import { authenticate } from "./store/session";
+import { ModalProvider } from "./context/Modal";
+
+
+function App() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(authenticate())
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ModalProvider>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/login" exact={true}>
+          <LoginForm />
+        </Route>
+        <Route path="/sign-up" exact={true}>
+          <SignUpForm />
+        </Route>
+        <ProtectedRoute path="/users" exact={true}>
+          <UsersList />
+        </ProtectedRoute>
+        <ProtectedRoute path="/users/:userId" exact={true}>
+          <User />
+        </ProtectedRoute>
+        <Route path="/" exact={true}>
+          <h1>My Home Page</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    </ModalProvider>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/auth/LogoutButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../store/session';
+import { useHistory } from 'react-router-dom';
+
+const LogoutButton = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const onLogout = async (e) => {
+    await dispatch(logout())
+      .then(history.push('/'))
+  };
+
+  return <div className='log-out' onClick={onLogout}>Logout</div>;
+};
+
+export default LogoutButton;
