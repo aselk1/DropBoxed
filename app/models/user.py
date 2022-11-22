ad7682545e948d1bea6dc04eb2c90e0d11895522
AdamSelki <adamselki@yahoo.com>
2022-11-22 13:02:51 -0800

    inital app folder for backend

---
+++ app/models/user.py
@@ -0,0 +1,82 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+# follows = db.Table(
+#     "follows",
+#     db.Column("follower_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
+#     db.Column("followed_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), primary_key=True),
+# )
+
+# like_story = db.Table(
+#     "like_story",
+#     db.Column("user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id"))),
+#     db.Column("story_id", db.Integer, db.ForeignKey(add_prefix_for_prod("stories.id")))
+# )
+
+# like_comment = db.Table(
+#     "like_comment",
+#     db.Column("user_id", db.Integer, db.ForeignKey(add_prefix_for_prod("users.id"))),
+#     db.Column("comment_id", db.Integer, db.ForeignKey(add_prefix_for_prod("comments.id")))
+# )
+
+if environment == "production":
+    pass
+    # follows.schema = SCHEMA
+    # like_story.schema = SCHEMA
+    # like_comment.schema = SCHEMA
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+
+
+    # followers = db.relationship(
+    #     "User",
+    #     secondary=follows,
+    #     primaryjoin=(follows.c.followed_id == id),
+    #     secondaryjoin=(follows.c.follower_id == id),
+    #     backref=db.backref("following", lazy="dynamic"),
+    #     lazy="dynamic",
+    # )
+
+    # liked = db.relationship(
+    #     "Story",
+    #     secondary=like_story,
+    #     lazy='dynamic',
+    #     back_populates = "liked_story_user"
+    # )
+
+    # liked_comment = db.relationship(
+    #     "Comment",
+    #     secondary=like_comment,
+    #     lazy='dynamic',
+    #     back_populates = "liked_comment_user"
+    # )
+
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+        }
---
+++ app/seeds/comment.py
@@ -0,0 +1,36 @@
+from app.models import db, User, environment, SCHEMA, Comment
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_comments():
+    comment1 = Comment(body='Good read.', user_id=1,story_id=2)
+    comment2 = Comment(body='Nice.', user_id=2,story_id=4)
+    comment3 = Comment(body='Great.', user_id=3,story_id=5)
+    comment4 = Comment(body='Interesting.', user_id=4,story_id=1)
+    comment5 = Comment(body='Bad idea!', user_id=5,story_id=1)
+    comment6 = Comment(body='Good idea!', user_id=6,story_id=2)
+   
+
+    db.session.add(comment1)
+    db.session.add(comment2)
+    db.session.add(comment3)
+    db.session.add(comment4)
+    db.session.add(comment5)
+    db.session.add(comment6)
+    db.session.commit()
+
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_comments():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM comments")
+        
+    db.session.commit()
