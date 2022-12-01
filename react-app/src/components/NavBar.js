0fd39fb7fda0bc4a322a3c0b68243f0ee5ace010
AdamSelki <adamselki@yahoo.com>
2022-12-01 14:37:35 -0800

    basic splash page

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -6,57 +6,62 @@ import "../index.css";
 
 const NavBar = ({ user, loaded }) => {
   return (
-    <div className="flexRow height48 alignCenter">
-      <div className="fixed widthFull whiteBack">
-      {loaded && (
-        <nav className="flexRow height48 justSpace widthFull">
-          <div className="flexRow alignCenter">
-            <NavLink to="/" exact={true} activeClassName="active">
-              <img src={favicon}></img>
-            </NavLink>
-            <NavLink to="/" exact={true} activeClassName="active">
-              <div className="height48 flexRow alignCenter">
-                <div className="logoText">DropBoxed</div>
-              </div>
-            </NavLink>
-          </div>
-          <ul className="flexRow noBullets alignCenter">
-            {/* <li key="home">
+    <div className="flexRow height48 alignCenter bottomMargin">
+      <div className="fixed widthFull whiteBack plainBorder">
+        {loaded && (
+          <nav className="flexRow height48 justSpace widthFull">
+            <div className="flexRow alignCenter">
+              <NavLink
+                to="/"
+                exact={true}
+                activeClassName="active"
+                className="height48"
+              >
+                <img src={favicon}></img>
+              </NavLink>
+              <NavLink to="/" exact={true} activeClassName="active">
+                <div className="height48 flexRow alignCenter">
+                  <div className="logoText">DropBoxed</div>
+                </div>
+              </NavLink>
+            </div>
+            <ul className="flexRow noBullets alignCenter">
+              {/* <li key="home">
             <NavLink to="/" exact={true} activeClassName="active">
               Home
             </NavLink>
           </li> */}
-            {!user && (
-              <li key="login">
-                <NavLink to="/login" exact={true} activeClassName="active">
-                  <div className="sign flexRow alignCenter justCenter">
-                    Sign in
-                  </div>
-                </NavLink>
-              </li>
-            )}
-            {!user && (
-              <li key="sing-up">
-                <NavLink to="/sign-up" exact={true} activeClassName="active">
-                  <div className="sign flexRow alignCenter justCenter">
-                    Sign up
-                  </div>
-                </NavLink>
-              </li>
-            )}
-            {/* <li key="users">
+              {!user && (
+                <li key="login">
+                  <NavLink to="/login" exact={true} activeClassName="active">
+                    <div className="sign flexRow alignCenter justCenter">
+                      Sign in
+                    </div>
+                  </NavLink>
+                </li>
+              )}
+              {!user && (
+                <li key="sing-up">
+                  <NavLink to="/sign-up" exact={true} activeClassName="active">
+                    <div className="sign flexRow alignCenter justCenter">
+                      Sign up
+                    </div>
+                  </NavLink>
+                </li>
+              )}
+              {/* <li key="users">
           <NavLink to="/users" exact={true} activeClassName="active">
             Users
           </NavLink>
         </li> */}
-            {user && (
-              <li key="logout">
-                <LogoutButton />
-              </li>
-            )}
-          </ul>
-        </nav>
-      )}
+              {user && (
+                <li key="logout">
+                  <LogoutButton />
+                </li>
+              )}
+            </ul>
+          </nav>
+        )}
       </div>
     </div>
   );
--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -1,5 +1,20 @@
+import HomePage from "./images/HomePage.png";
+
 const Splash = () => {
-  return <div>My Splash Page</div>;
+  return (
+    <div className="flexCol alignCenter splashBack">
+      <div className="splashBack2">
+        <h2 className="fontHead3">
+          Join over 700 million registered users who trust DropBoxed
+        </h2>
+        <h3 className="fontHead4">
+          Easy to use, reliable, private, and secure. It’s no wonder DropBoxed is
+          the choice for storing and sharing your most important files.
+        </h3>
+      </div>
+        <img src={HomePage} className="homePage"></img>
+    </div>
+  );
 };
 
 export default Splash;
